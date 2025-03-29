6ff11a313cb6a9712826c5b3f3eeb0442d5328fd
jesusking2722 <jesusking2722@gmail.com>
2025-03-30 02:57:43 +0900

    fix port

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.use(cors());
 
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT | 5001;
+const PORT = process.env.PORT || 5001;
 
-app.listen(() => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
